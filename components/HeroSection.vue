573869b19f902761bce7194aec0e1f9d56b3bd21
tkohlmannhuber <thomas.kohlmannhuber@yahoo.de>
2021-12-16 15:08:44 +0100

    add hero Section

---
+++ components/HeroSection.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="grid place-items-center">
+    <h1 class="font-text text-7xl">Towa Pizza List</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+div {
+  height: 50vh;
+}
+</style>
