9cfe89b24abbaa9e90dce3dcd826f03742a2324c
tkohlmannhuber <thomas.kohlmannhuber@yahoo.de>
2021-12-16 14:42:16 +0100

    add search function

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,16 @@
 <template>
-  <div class="px-5">
-    <PizzaList :pizzas="pizzas" />
+  <div class="px-5 h-screen grid place-items-center">
+    <label for="search">
+      <input
+        id="search"
+        v-model="search"
+        class="border-2 border-black px-5 py-2"
+        type="text"
+        name="search"
+        placeholder="Search for Pizza"
+      />
+    </label>
+    <PizzaList :pizzas="searchPizza" />
   </div>
 </template>
 
@@ -14,6 +24,22 @@ export default {
       'http://pizza.test/wp-json/pizza/v1/pizzas'
     )
     return { pizzas: response.data }
+  },
+
+  data() {
+    return {
+      search: '',
+    }
+  },
+  computed: {
+    searchPizza() {
+      const pizza = this.pizzas.filter((singlePizza) => {
+        return singlePizza?.post_name
+          .toLowerCase()
+          .includes(this.search.toLowerCase())
+      })
+      return pizza.length > 0 ? pizza : this.pizzas
+    },
   },
 }
 </script>
