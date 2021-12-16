d526f241385ba76b81cdfe141d5a014df24a7604
tkohlmannhuber <thomas.kohlmannhuber@yahoo.de>
2021-12-16 11:46:53 +0100

    add pizza list to list all pizzas

---
+++ components/PizzaList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="grid">
+    <PizzaCard v-for="pizza in pizzas" :key="pizza.id" :pizza="pizza" />
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    pizzas: {
+      type: Array,
+      default: () => [],
+    },
+  },
+}
+</script>
