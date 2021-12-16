3327996becf241b57bc8829ed2cdd4eb29a444ef
tkohlmannhuber <thomas.kohlmannhuber@yahoo.de>
2021-12-16 15:23:02 +0100

    change responsive font size

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="grid place-items-center">
-    <h1 class="font-text text-7xl">Towa Pizza List</h1>
+    <h1 class="font-text text-5xl text-center lg:text-7xl">TOWA Pizza List</h1>
   </div>
 </template>
 
