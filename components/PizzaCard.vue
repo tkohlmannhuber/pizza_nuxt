929848a4932b24162f0a4d5fefdbdc54f3b2e481
tkohlmannhuber <thomas.kohlmannhuber@yahoo.de>
2021-12-16 11:46:33 +0100

    add pizza card for pizza details output

---
+++ components/PizzaCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <img :src="pizza.acf.image.url" alt="" />
+    <h2>{{ pizza.post_name }}</h2>
+    <p>{{ pizza.acf.ingredients }}</p>
+    <p>{{ pizza.acf.price }} €</p>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    pizza: {
+      type: Object,
+      default: () => {},
+    },
+  },
+}
+</script>
