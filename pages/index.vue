d7f08f513993f8d11a93cd70b17197ae0ff2a96b
tkohlmannhuber <thomas.kohlmannhuber@yahoo.de>
2021-12-16 11:47:30 +0100

    add pizza list to show pizza list

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,19 @@
 <template>
-  <Tutorial />
+  <div class="px-5">
+    <PizzaList :pizzas="pizzas" />
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'IndexPage',
+
+  async asyncData() {
+    const response = await axios.get(
+      'http://pizza.test/wp-json/pizza/v1/pizzas'
+    )
+    return { pizzas: response.data }
+  },
 }
 </script>
