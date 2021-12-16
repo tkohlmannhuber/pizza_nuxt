f29fa97e6e4ada89c38b9f8a0bc32f492b667079
tkohlmannhuber <thomas.kohlmannhuber@yahoo.de>
2021-12-16 12:27:29 +0100

    image output + styling

--- components/PizzaCard.vue
+++ components/PizzaCard.vue
@@ -1,10 +1,23 @@
 <template>
-  <div>
-    <img :src="pizza.acf.image.url" alt="" />
-    <h2>{{ pizza.post_name }}</h2>
-    <p>{{ pizza.acf.ingredients }}</p>
-    <p>{{ pizza.acf.price }} €</p>
-  </div>
+  <a
+    href="#"
+    class="bg-white px-10 pb-10 grid gap-5 shadow-2xl rounded-xl hover:shadow-2xl"
+  >
+    <figure
+      class="relative rounded-full h-36 w-36 transform -translate-y-1/4 shadow-2xl justify-self-center"
+    >
+      <img
+        :src="pizza.acf.image.url"
+        alt=""
+        class="absolute top-0 left-0 rounded-full h-full w-100 object-cover"
+      />
+    </figure>
+    <h2 class="text-3xl font-text capitalize font-bold">
+      {{ pizza.post_name }}
+    </h2>
+    <p class="font-text font-medium">{{ pizza.acf.ingredients }}</p>
+    <p class="font-text font-bold text-2xl">{{ pizza.acf.price }} €</p>
+  </a>
 </template>
 <script>
 export default {
